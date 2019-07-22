228b5b791f27c9f65e3c5e11e5155f8f66822cd7
chrisdalao <chrisdalao@gmail.com>
2019-07-22 13:16:15 -0700

    real world file structures and file naming convention implemented

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// const authRouter = require('./auth/auth-router');
+
+const server = express();
+
+server.use(express.json());
+// server.use('/api/auth', authRouter);
+
+server.get('/', (req, res) => {
+    res.send('<h1>Auth Challenge</h1>')
+})
+
+module.exports = server;
